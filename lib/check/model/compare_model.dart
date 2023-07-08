47f0bd938d0aaf92d699a2c7c522a8ced0e1f245
alrik.schnapke <itnwsazo4jsu4be6njuxtzafqitxsgjfz5cjl3exmavdm5lg2i6a>
2023-07-08 12:36:21 +0200

    implement Regex

---
+++ lib/check/model/compare_model.dart
@@ -0,0 +1,7 @@
+class Compare {
+  String type;
+  String operator;
+  String value;
+
+  Compare({required this.type, required this.operator, required this.value});
+}
--- lib/check/service/check_service.dart
+++ lib/check/service/check_service.dart
@@ -2,20 +2,27 @@ import 'dart:io';
 
 import 'package:titawin/application/contraints.dart';
 import 'package:titawin/check/model/check_model.dart';
+import 'package:titawin/check/model/compare_model.dart';
 
 abstract class ICheckService {
-  Future<Check> executeCheck(String cmd, String compare, String value,
-      String type, String okText, String notOkText);
+  Future<Check> executeCheck(String cmd, Compare compare, String type,
+      String okText, String notOkText);
 }
 
 class CheckService implements ICheckService {
   @override
-  Future<Check> executeCheck(String cmd, String compare, String value,
-      String type, String okText, String notOkText) async {
+  Future<Check> executeCheck(String cmd, Compare compare, String type,
+      String okText, String notOkText) async {
+    var tmp = false;
     var result = await Process.run(mapType(type), [cmd]);
 
-    var tmp =
-        calc(int.parse(result.stdout.toString()), compare, int.parse(value));
+    if (compare.type == "number") {
+      tmp = calcNumber(int.parse(result.stdout.toString()), compare.operator,
+          int.parse(compare.value));
+    } else if (compare.type == "regex") {
+      tmp = calcRegex(result.stdout.toString(), compare.value);
+    }
+
     if (tmp) {
       return Check(isOk: true, message: okText);
     } else {
@@ -29,7 +36,7 @@ class CheckService implements ICheckService {
     throw Exception("Type not found");
   }
 
-  bool calc(int real, String compare, int value) {
+  bool calcNumber(int real, String compare, int value) {
     switch (compare) {
       case "=":
         return real == value;
@@ -47,5 +54,9 @@ class CheckService implements ICheckService {
       default:
         return false;
     }
+  }
+
+  bool calcRegex(String regex, String patter) {
+    return RegExp(patter).hasMatch(regex);
   }
 }
